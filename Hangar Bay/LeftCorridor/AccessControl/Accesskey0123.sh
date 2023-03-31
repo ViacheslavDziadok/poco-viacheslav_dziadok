deb80eb0e4ff5d7339feaa96c657669e9d4c348e
“hsynsvndk” <hsynsvndk@hotmail.com>
2023-03-31 15:32:50 +0200

    access control done

---
+++ Hangar Bay/LeftCorridor/AccessControl/Accesskey0123.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ echo "Congratulations! You have unlocked the secret key. "
+ echo "Secret Key ==> SLH"
+
+  echo "Congratulations! You have unlocked the secret key. "
--- Hangar Bay/Right Corridor/RealPhaserSystem/RealPhaserSystem.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "In 1 minute you have to make a choice."
-for i in {5..1}; do
-    echo "$i seconds remaining"
-    sleep 1
-done
-echo "Congratulations Agent 007 ! ! !"
