81fe892f4be1350986d0879774c68cdf819b791c
“hsynsvndk” <hsynsvndk@hotmail.com>
2023-03-31 15:41:25 +0200

    access control is done

--- Hangar Bay/LeftCorridor/AccessControl/Accesskey0123.sh
+++ Hangar Bay/LeftCorridor/AccessControl/Accesskey0123.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
  echo "Congratulations! You have unlocked the secret key. "
- echo "Secret Key ==> SLH"
+ echo "Secret Key ==> SLHAHU"
 
   echo "Congratulations! You have unlocked the secret key. "
